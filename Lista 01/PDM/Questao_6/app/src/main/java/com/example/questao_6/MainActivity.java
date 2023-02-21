03447c365efb6179913ed3c7b941d26671d6b7ee
Cauã Mistura <cauamistura1@gmail.com>
2023-02-20 22:13:20 -0300

    update lista 01

--- Lista 01/PDM/Questao_6/app/src/main/java/com/example/questao_6/MainActivity.java
+++ Lista 01/PDM/Questao_6/app/src/main/java/com/example/questao_6/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.questao_6;
 
-import android.support.v7.app.AppCompatActivity;
+import static java.lang.String.valueOf;
+
+import android.app.ActivityOptions;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
                 lbCount.setText("Fui Clicado!!");
             }
         });
+
+        final Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final TextView lbNum = (TextView) findViewById(R.id.lbnum);
+                final EditText edN1 = (EditText) findViewById(R.id.edn1);
+                final EditText edN2 = (EditText) findViewById(R.id.edn2);
+                Integer n1 = Integer.valueOf(valueOf(edN1.getText()));
+                Integer n2 = Integer.valueOf(valueOf(edN2.getText()));
+                lbNum.setText(valueOf(getRandomInt(n1,n2)));
+            }
+        });
+    }
+    public static int getRandomInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
     }
 }
