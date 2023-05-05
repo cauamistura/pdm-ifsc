cf8a2a99ff1caa88cf9d09a10c7f9951b233f8be
Caua <cauamistura1@gmail.com>
2023-05-05 13:19:50 -0300

    Correção lista

--- aulas/aula07/app/src/main/java/br/com/ifsc/aula07/MainActivity.java
+++ aulas/aula07/app/src/main/java/br/com/ifsc/aula07/MainActivity.java
@@ -1,11 +1,15 @@
 package br.com.ifsc.aula07;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,17 +29,33 @@ public class MainActivity extends AppCompatActivity {
         pm = getPackageManager();
 
         //Pegando nomes dos apps instalados no sistemas
-        ArrayList<String> appsNames= new ArrayList<>();
+        ArrayList<String> appsNames = new ArrayList<>();
         List<ApplicationInfo> apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        for (ApplicationInfo app: apps) {
+        for (ApplicationInfo app : apps) {
             appsNames.add(app.loadLabel(pm).toString());
         }
 
         //Criando adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, appsNames);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, appsNames);
 
         //Adaptador Personalizado
         AppsAdapter adapterPersonalizado = new AppsAdapter(this, R.layout.list_layout, apps);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ApplicationInfo applicationInfo = (ApplicationInfo) adapterView.getItemAtPosition(i);
+                String packageName = applicationInfo.packageName;
+                Toast.makeText(MainActivity.this, "Abrindo o App", Toast.LENGTH_SHORT).show();
+
+                Intent intent = pm.getLaunchIntentForPackage(packageName);
+                if (intent != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Não foi possível abrir o aplicativo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         //Setando o adapter
         list.setAdapter(adapterPersonalizado);
